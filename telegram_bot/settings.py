81a6acf0a86b47dfa57ad3acd801aad3af78726a
Muzych <haxk1024@gmail.com>
2023-12-22 11:56:22 +0000

    feat: delete all files

--- telegram_bot/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings, SecretStr
-
-class Settings(BaseSettings):
-    TELEGRAM_BOT_API_TOKEN: SecretStr
-
-    class Config:
-        env_file = "../.env"
-        env_file_encoding = "utf-8"
